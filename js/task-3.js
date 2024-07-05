5cd93dbf250f6ae732d35abd67011e26359ec9c4
Yarik2019 <yroslavterno@gmail.com>
2024-07-05 22:25:01 +0300

    add gellary

--- js/task-3.js
+++
@@ -1,8 +0,0 @@
-const nameInput = document.getElementById("name-input");
-const nameOutput = document.getElementById("name-output");
-
-nameInput.addEventListener("input", () => {
-  const inputValue = nameInput.value.trim();
-
-  nameOutput.textContent = inputValue === "" ? "Anonymous" : inputValue;
-});
